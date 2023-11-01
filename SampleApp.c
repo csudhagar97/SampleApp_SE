bbfb0ca27d7f8300b9d80637b611ea65afcee3c2
Sudhagar Chinnathambi <72736222+csudhagar97@users.noreply.github.com>
2023-11-01 20:26:02 +0530

    Update SampleAppv1
    commit changes

--- SampleApp.c
+++ SampleApp.c
@@ -1,1 +1,51 @@
-SampleApp_SE
+#include <iostream>
+#include <cstring>
+#include <unordered_map>
+
+using namespace std;
+
+unordered_map<string, string> users = {
+    {"admin", "admin_pass"},
+    {"user", "user_pass"}
+};
+
+void getUserInput(char *buffer) {
+    cout << "Enter input: ";
+    cin >> buffer;
+}
+
+int main() {
+    char username[10];
+    char password[10];
+
+    cout << "Enter username: ";
+    cin >> username;
+
+    cout << "Enter password: ";
+    cin >> password;
+
+    if (users.find(username) != users.end() && users[username] == password) {
+        cout << "Authentication successful! Welcome " << username << "!\n";
+
+        char buffer[50];
+
+        cout << "Enter a command to execute: ";
+        cin.ignore();
+        cin.getline(buffer, sizeof(buffer));
+        system(buffer);
+
+        cout << "Enter the username whose password you want to view: ";
+        char viewUsername[10];
+        cin >> viewUsername;
+
+        if (users.find(viewUsername) != users.end()) {
+            cout << "Password for " << viewUsername << " is: " << users[viewUsername] << "\n";
+        } else {
+            cout << "User " << viewUsername << " does not exist!\n";
+        }
+    } else {
+        cout << "Invalid username or password!\n";
+    }
+
+    return 0;
+}
